13fa1fd2fd0e918beabe419f0e3e5ee98984f1c1
高俊杰 <gaojunjie@sunline.cn>
2021-02-08 14:25:47 +0800

    auth

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,5 @@
+const githubClientId = '07cd50aded5f5283df67';
+const githubClientSecret = 'f75b4c01dc14466e3026fc27132caaef501a082a';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
